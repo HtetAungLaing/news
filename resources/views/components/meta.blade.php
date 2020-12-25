fe8413d0ee86e38c7033f0efae0b217674da511a
HtetAungLaing <myueway98@gmail.com>
2020-12-26 04:14:10 +0630

    fa

--- resources/views/components/meta.blade.php
+++ resources/views/components/meta.blade.php
@@ -1,3 +1,4 @@
-<meta property="op:markup_version" content="v1.0">>
+<meta property="op:markup_version" content="v1.0">
 <meta property="fb:article_style" content="myarticlestyle">
-<meta name="propeller" content="704dde846cfb678f354a6edd028d8eb6" {{ $slot }}
+<meta name="propeller" content="526b1263c9338371a02cd56fe6281c0f">
+{{ $slot }}
