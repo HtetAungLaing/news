236c36c709620ef9df37a612b3e3511be878b2a0
HtetAungLaing <myueway98@gmail.com>
2020-12-26 02:11:09 +0630

    1fina2asl

--- resources/views/components/css.blade.php
+++ resources/views/components/css.blade.php
@@ -2,3 +2,16 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
     crossorigin="anonymous">
 <link rel="stylesheet" href="{{ asset('vendor/feather-icons-web/feather.css') }}">
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-X7P5ESNQGW"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'G-X7P5ESNQGW');
+
+</script>
