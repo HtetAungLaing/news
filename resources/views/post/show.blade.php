5feb900b01cecce9995498d46139b37af1da4ae4
HtetAungLaing <myueway98@gmail.com>
2020-12-26 21:34:14 +0630

    final

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>အင်ကြင်းသတင်း</title>
+    <title>အော်ရိုရာသတင်း</title>
     @component('components.meta')
         <link rel="canonical" href="http://www.auroranews.online/post/{{ $post->id }}">
         <!-- Primary Meta Tags -->
